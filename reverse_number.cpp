ba262695ebad1f8d251db966d56ebfa8767d0f53
Vikash Rajput <rajputvi448@gmail.com>
2026-02-06 17:32:22 +0530

    Add files via upload

---
+++ reverse_number.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+ using namespace std;
+
+int main()
+{
+    int n,i;
+    cout<<"enter the number"<<endl;
+    cin>>n;
+
+    for (i= n;i>=0;i=i-1)
+     {
+    cout<<i<<endl;  
+   }
+}
